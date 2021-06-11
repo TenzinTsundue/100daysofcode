5a0f0b07a8917d8664d5de314f02f2e89e9ec5f1
Tenzin Tsundue <40035716+TenzinTsundue@users.noreply.github.com>
2021-06-11 14:28:57 +0530

    day 16 class intro

---
+++ From day 11- 20/day16.py
@@ -0,0 +1,30 @@
+#object oriented programming
+
+#How to OOP
+#class(blueprint) has attributes(data) and  methods(functionality)
+#object is modeled from a class
+
+"""
+class CarBlueprint():    # declearing class
+	pass
+
+car = CarBlueprint()  # creating object
+"""
+
+# import turtle
+# timmy = turtle.Turtle()
+"""
+from turtle import Turtle, Screen
+timmy = Turtle()
+timmy.shape("turtle")
+timmy.color("green")
+timmy.forward(200)
+
+my_screen = Screen()
+# print(my_screen.canvheight)
+my_screen.exitonclick()
+"""
+
+
+
+
