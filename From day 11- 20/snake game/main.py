6a03c33c6b562bb0ece7cfa3f200c22142d8511b
Tenzin Tsundue <40035716+TenzinTsundue@users.noreply.github.com>
2021-06-13 20:47:45 +0530

    Day 20, snake game part I
    here we create snake class and its movement functions.

---
+++ From day 11- 20/snake game/main.py
@@ -0,0 +1,46 @@
+# snake game
+
+# create snake food
+# detect collision with food
+# create a score board
+# detect collision with wall
+# detect collision with tail
+
+from turtle import Turtle, Screen
+from snake import Snake
+import time
+ 
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Snake Game")
+screen.tracer(0)
+
+snake = Snake()
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+
+# move the snake
+game_is_on = True 
+while game_is_on:
+	screen.update()
+	time.sleep(0.1)
+
+	snake.move()
+
+
+
+
+
+screen.exitonclick()
+	
+
+
+
+	
